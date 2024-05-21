de650031654348a886e3f990608300416856c33a
elvesbd <elvesbd@yahoo.com>
2024-05-20 21:08:30 -0300

    chore: adding base configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppConfig } from '@config/app.config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+      }),
+    }),
+  ],
+  providers: [AppConfig],
 })
 export class AppModule {}
---
+++ src/config/setup-docs.ts
@@ -0,0 +1,21 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+
+export const setupDocs = (app: INestApplication): void => {
+  const config = new DocumentBuilder()
+    .setTitle('')
+    .setDescription('')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: '',
+      type: 'http',
+      bearerFormat: 'token',
+    })
+    .addTag('')
+    .addTag('')
+    .build();
+
+  const openAPIObj: OpenAPIObject = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, openAPIObj);
+};
