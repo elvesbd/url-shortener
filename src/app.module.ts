6eaa418bd89e917f1800a37ee79eda433777dfd9
elvesbd <elvesbd@yahoo.com>
2024-05-21 16:30:14 -0300

    chore: validate environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppConfig } from '@config/app.config';
+import { InfraModule } from '@infra/infra.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -8,9 +9,16 @@ import * as Joi from 'joi';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        PORT: Joi.number().default(3000),
+        PORT: Joi.number().required(),
+        JWT_SECRET: Joi.string().required(),
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
       }),
     }),
+    InfraModule,
   ],
   providers: [AppConfig],
 })
