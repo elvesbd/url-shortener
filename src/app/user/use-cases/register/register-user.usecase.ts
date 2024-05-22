67fa2f6506ecbe36e399848dde334860d69a0c1f
elvesbd <elvesbd@yahoo.com>
2024-05-22 07:44:52 -0300

    chore: creating use case to register user

--- src/app/user/use-cases/register/register-user.usecase.ts
+++ src/app/user/use-cases/register/register-user.usecase.ts
@@ -3,8 +3,8 @@ import { User } from '@app/user/domain/user';
 import { UseCase } from '@app/shared/interfaces/UseCase';
 import { UserFoundException } from '@app/user/exceptions/user-found.exception';
 import { RegisterUserInput } from './types/register-user.input';
-import { HasherPassword } from '@app/user/ports/hash/hash.password';
 import { UserRepository } from '@app/user/ports/repository/user.repository';
+import { HashPassword } from '@app/user/ports/cryptography/encrypt.password';
 
 type RegisterUserOutput = {
   data: User;
@@ -16,21 +16,21 @@ export class RegisterUserUseCase
   implements UseCase<RegisterUserInput, RegisterUserOutput>
 {
   constructor(
-    private readonly hasher: HasherPassword,
+    private readonly cryptography: HashPassword,
     private readonly userRepository: UserRepository,
   ) {}
 
   public async execute(input: RegisterUserInput): Promise<RegisterUserOutput> {
-    const userFoundByEmail = await this.userRepository.findByEmail(input.email);
+    const { name, email, password } = input;
+    const foundUser = await this.userRepository.findByEmail(email);
 
-    if (userFoundByEmail) {
-      throw new UserFoundException(input.email);
+    if (foundUser) {
+      throw new UserFoundException(email);
     }
 
-    const password = await this.hasher.hashPassword(input.password);
-
     const user = User.create({
-      ...input,
+      name,
+      email,
       password,
     });
 
@@ -40,6 +40,9 @@ export class RegisterUserUseCase
         notifications: user.notifications,
       };
     }
+
+    const hash = await this.cryptography.hash(password);
+    user.setPasswordWithHash(hash);
 
     await this.userRepository.register(user);
     return {
