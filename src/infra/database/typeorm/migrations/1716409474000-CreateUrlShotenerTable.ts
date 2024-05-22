80148c698f2a9648a50696e32ef49a55bbf48f60
elvesbd <elvesbd@yahoo.com>
2024-05-22 17:53:12 -0300

    chore: creating shortener url table

---
+++ src/infra/database/typeorm/migrations/1716409474000-CreateUrlShotenerTable.ts
@@ -0,0 +1,74 @@
+import { MigrationInterface, QueryRunner, Table, TableIndex } from 'typeorm';
+
+export class CreateUrlShotenerTable1716409474000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'url_shorteners',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'shortUrl',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'originalUrl',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'accessCount',
+            type: 'int',
+            default: 0,
+          },
+          {
+            name: 'userId',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+            onUpdate: 'CURRENT_TIMESTAMP',
+            isNullable: true,
+          },
+          {
+            name: 'deletedAt',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+      true,
+    );
+
+    await queryRunner.createIndex(
+      'url_shorteners',
+      new TableIndex({
+        name: 'IDX_SHORT_URL',
+        columnNames: ['shortUrl'],
+        isUnique: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropIndex('url_shorteners', 'IDX_SHORT_URL');
+    await queryRunner.dropTable('url_shorteners');
+  }
+}
