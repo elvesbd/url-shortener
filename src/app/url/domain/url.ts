c420865af3f652589a5b24a7e6f47336108daa01
elvesbd <elvesbd@yahoo.com>
2024-05-22 17:50:28 -0300

    chore: implementing url shortener domain

--- src/app/url/domain/url.ts
+++ src/app/url/domain/url.ts
@@ -1,25 +1,28 @@
 import { Entity } from '@app/shared/entity/entity';
-import { Notification } from '@app/shared/notification/notification';
 import { Url } from '@app/shared/value-objects/url';
-
-type UrlProps = {
-  originalUrl: string;
-  shortUrl?: string;
-};
+import { Notification } from '@app/shared/notification/notification';
+import { UrlProps } from './types/url-props';
 
 export class UrlShortener extends Entity<UrlProps> {
+  private _userId?: string;
   private _originalUrl: Url;
   private _shortUrl: string;
-  public readonly updatedAt: Date;
-  public readonly deletedAt: Date;
+  private _accessCount: number;
+  private _deletedAt?: Date | null;
+  public readonly updatedAt?: Date;
   public readonly notification: Notification;
+  private readonly baseDomain: string = 'http://localhost';
 
   constructor(props: UrlProps) {
     super(props);
     this.notification = new Notification();
 
-    this._originalUrl = new Url(props.originalUrl, this.notification);
+    this._userId = props.userId;
+    this.updatedAt = props.updatedAt;
+    this._deletedAt = props.deletedAt ?? null;
+    this._accessCount = props.accessCount;
     this._shortUrl = props.shortUrl ?? this.generateShortUrl();
+    this._originalUrl = new Url(props.originalUrl, this.notification);
   }
 
   static create(props: UrlProps): UrlShortener {
@@ -31,7 +34,39 @@ export class UrlShortener extends Entity<UrlProps> {
   }
 
   get shortUrl(): string {
-    return this._shortUrl;
+    return `${this.baseDomain}/${this._shortUrl}`;
+  }
+
+  get userId(): string | undefined {
+    return this._userId;
+  }
+
+  get accessCount(): number {
+    return this._accessCount;
+  }
+
+  get deletedAt(): Date {
+    return this._deletedAt;
+  }
+
+  get hasNotifications(): boolean {
+    return this.notification.hasNotifications();
+  }
+
+  get notifications(): string[] {
+    return this.notification.getNotifications();
+  }
+
+  public updateDestination(newOriginalUrl: string): void {
+    this._originalUrl = new Url(newOriginalUrl, this.notification);
+  }
+
+  public incrementAccessCount(): void {
+    this._accessCount++;
+  }
+
+  public markAsDeleted(): void {
+    this._deletedAt = new Date();
   }
 
   private generateShortUrl(length: number = 6): string {
